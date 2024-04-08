abcf0ef832df77667e83049ea99a0d8ad3954a55
Myron Ladyjenko <mladyjen@uoguelph.ca>
2024-04-08 18:01:55 -0400

    feature/SendVoteTransaction - finished BSP

--- code/blockchain/helper.py
+++ code/blockchain/helper.py
@@ -1,6 +1,10 @@
 import random
 import string
 from hashlib import sha256
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
 
 # Generates a random string
 def generate_random_string(length):
@@ -29,3 +33,42 @@ def compute_sha256_hash(hex_string):
     hex_digest = hash_object.hexdigest()
 
     return hex_digest
+
+def encrypt_rsa(message, pb_key):
+    encrypted_message = pb_key.encrypt(
+        message.encode(),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    return encrypted_message    
+
+def decrypt_rsa(enc_message, pr_key):
+    # Decrypt the message using RSA-OAEP padding
+    decrypted_message = pr_key.decrypt(
+        enc_message,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    return decrypted_message.decode()
+
+def generate_signature(hash_value, private_key):
+
+    # Sign the hash value using the private key
+    signature = private_key.sign(
+        hash_value,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+
+    return signature
